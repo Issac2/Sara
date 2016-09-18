17e2f2db8db946f84c1a8b5611c2e609b1873bca
somnus <somnus@qq.com>
2016-09-18 14:25:19 +0800

    完善

--- sara-web/src/main/java/com/somnus/sara/web/controller/MerAccountController.java
+++ sara-web/src/main/java/com/somnus/sara/web/controller/MerAccountController.java
@@ -1,4 +1,4 @@
-package com.somnus.sara.controller;
+package com.somnus.sara.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
